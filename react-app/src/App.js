9d262da3e50998b0df1cdd0ae1c52946e5640d99
Surma <Bartosz.Surma@aecom.com>
2022-08-25 11:38:46 +0200

    changed router to switch

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import PokemonCard from "./components/PokemonCard/PokemonCard.jsx"
-import { Routes, Route } from "react-router-dom"
+import { Switch, Route } from "react-router-dom"
 import { useState, useEffect } from "react"
 import { AnimatePresence } from "framer-motion"
 
@@ -23,7 +23,7 @@ function App() {
 
     return (
         <AnimatePresence>
-                <Route key="home" path="/" element={<Home history={history} setHistory={setHistory} />} />
+            <Switch>
                 <Route
                     key="pokedex"
                     path="/pokedex"
@@ -48,6 +48,8 @@ function App() {
                         />
                     }
                 />
+                <Route key="home" path="/" element={<Home history={history} setHistory={setHistory} />} />
+            </Switch>
         </AnimatePresence>
     )
 }

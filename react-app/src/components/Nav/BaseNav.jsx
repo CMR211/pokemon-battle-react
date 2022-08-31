ad8c494c9964900f2542fc399cfcae20cbdad3b4
Surma <Bartosz.Surma@aecom.com>
2022-08-31 12:17:25 +0200

    BaseNav refactor

--- react-app/src/components/Nav/BaseNav.jsx
+++ react-app/src/components/Nav/BaseNav.jsx
@@ -1,23 +1,31 @@
+// Libraries
 import React from "react"
 import { useNavigate } from "react-router-dom"
 
+// Utilities
 import { goToLocation } from "../../utilities/goToLocation"
 import { returnToLocation } from "../../utilities/returnToLocation"
 
+// Icons
 import IconHome from "../../icons/IconHome"
 import IconReturn from "../../icons/IconReturn"
 
 export default function BaseNav({ history, setHistory, currentLocation }) {
-    const navigate = useNavigate()
+    // Legacy class changer
     const currentClass = "nav"
+
+    // Navigation handlers
+    const navigate = useNavigate()
     function goToHome() {
         goToLocation("/", currentLocation, setHistory, navigate)
     }
+    function goToPrevious() {
+        returnToLocation(navigate, history, setHistory)
+    }
+
     return (
         <div className={`${currentClass}`}>
-            <button
-                className={`${currentClass}__button--return ${currentClass}__button`}
-                onClick={() => returnToLocation(navigate, history, setHistory)}>
+            <button className={`${currentClass}__button--return ${currentClass}__button`} onClick={goToPrevious}>
                 <IconReturn />
             </button>
             <button className={`${currentClass}__button--home ${currentClass}__button`} onClick={goToHome}>
